5dedb31b1890644b36fb2f1d8b04e62cfab50754
Thong <thongnguyen1292@gmail.com>
2017-02-24 22:17:09 +0100

    Location management

---
+++ Algorithms/DivideAndConquer/Week3/PivotFirstEle.py
@@ -0,0 +1,13 @@
+def swap(A,i,j):
+    # Swap A[i] and A[j]
+
+
+def QuickSort(A, n):
+    if n == 1 return
+    p = ChoosePivot(A,n)
+
+
+
+with open("QuickSort.txt") as fin:
+    ele = fin.read().split('\n')
+
